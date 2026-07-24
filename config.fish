359559544e51d63350894fb381de3ab6345fc346
C01NoP <mnovak430@gmail.com>
2026-07-24 15:30:01 -0500

    fish.conf update

--- config.fish
+++ config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
 alias hc="history clear"
 alias fru="flatpak remove --unused"
+alias multipull="cd ~/Git; find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;"
 function cleantn; rm -rf ~/.cache/thumbnails/normal/*; end
 function cleanr; rm ~/.local/share/recently-used.xbel; end
 alias wads="cd /home/mike/wads"
 alias bat="batcat"
 alias mobo="cat /sys/devices/virtual/dmi/id/board_{vendor,name,version}"
 alias birthday="stat -c %w /"
-alias fishsource="source ~/.config/fish/config.fish"
 alias fishconf="vim ~/.config/fish/config.fish"
 alias timefix="sudo hwclock --systohc"
 end
